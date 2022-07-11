fddecaae8cf0c1743b31bd42dbf1ffd36f91880f
Clesio ruela <clesioruela1@gmail.com>
2022-07-11 16:39:00 -0300

    mudança para aprender Github 11/07/2022

--- geek/src/avancados/ComparadorPorTamanho.java
+++ geek/src/avancados/ComparadorPorTamanho.java
@@ -7,9 +7,9 @@ public class ComparadorPorTamanho implements Comparator<String> {
 	@Override
 	public int compare(String st1, String st2) {
 		if (st1.length() < st2.length()) {
-			return -1;
-		} else if (st1.length() > st2.length()) {
 			return 1;
+		} else if (st1.length() > st2.length()) {
+			return -1;
 		} else {
 			return 0;			
 		}
